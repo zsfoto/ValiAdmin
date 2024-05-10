853c1dd140a1ab0ccc52c90002855c4f4b98ca8c
Jeff Shoemaker <zsfoto@gmail.com>
2024-05-10 15:55:56 +0200

    Only the daily backup

--- templates/element/sidebar_menu.php
+++ templates/element/sidebar_menu.php
@@ -2,30 +2,29 @@
 	<div class="app-sidebar__overlay" data-toggle="sidebar"></div>
 	<aside class="app-sidebar">
 	
-		<div class="app-sidebar__user"><img class="app-sidebar__user-avatar" src="https://randomuser.me/api/portraits/men/1.jpg" alt="User Image">
-			<div>
-				<p class="app-sidebar__user-name">John Doe</p>
-				<p class="app-sidebar__user-designation">Frontend Developer</p>
+		<div class="app-sidebar__user"><?= $this->Html->image('ValiAdmin./img/avatar.png', ['class' => 'app-sidebar__user-avatar', 'alt' => 'User Image']) ?>
+		<div>
+				<p class="app-sidebar__user-name"><?= __('Jeff Shoemaker') ?></p>
+				<p class="app-sidebar__user-designation"><?= __('Fullstack Developer') ?></p>
 			</div>
 		</div>
 		
 		<ul class="app-menu">
 			
-			<li>
-				<a class="app-menu__item" href="dashboard.html">
-					<i class="app-menu__icon bi bi-speedometer"></i>
-					<span class="app-menu__label">Dashboard</span>
-				</a>
-			</li>
+			<li><?= $this->Html->link('<i class="app-menu__icon bi bi-speedometer"></i><span class="app-menu__label">' . __('Posts') . '</span>', ['controller' => 'Posts', 'action' => 'index'], ['class' => 'app-menu__item', 'escapeTitle' => false]) ?></li>
 			
 			<li class="treeview">
+			
 				<a class="app-menu__item" href="#" data-toggle="treeview">
-					<i class="app-menu__icon bi bi-laptop"></i><span class="app-menu__label">UI Elements</span>
-					<i class="treeview-indicator bi bi-chevron-right"></i>
+					<i class="app-menu__icon bi bi-laptop"></i><span class="app-menu__label"><?= __('Admin') ?></span><i class="treeview-indicator bi bi-chevron-right"></i>
 				</a>
+
+				
 				<ul class="treeview-menu">
 					<li><a class="treeview-item" href="bootstrap-components.html"><i class="icon bi bi-circle-fill"></i> Bootstrap Elements</a></li>
 					<li>
+						<?= $this->Html->link('<i class="app-menu__icon bi bi-speedometer"></i><span class="app-menu__label">' . __('Creators') . '</span>', ['controller' => 'Creators', 'action' => 'index'], ['class' => 'treeview-item', 'escapeTitle' => false]) ?>
+
 						<a class="treeview-item" href="https://icons.getbootstrap.com/" target="_blank" rel="noopener"><i class="icon bi bi-circle-fill"></i>
 							Font Icons
 						</a>
