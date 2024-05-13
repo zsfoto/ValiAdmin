a0b989a55d586f037002805faa966bfb272a97bc
Jeff Shoemaker <zsfoto@gmail.com>
2024-05-13 14:31:32 +0200

    Bake template hegesztése

--- templates/element/app_title.php
+++ templates/element/app_title.php
@@ -4,8 +4,11 @@
 				<h1><i class="<?= $this->fetch('AppTitleIcon') ?>"></i> <?= $this->fetch('AppTitle') ?></h1>
 				<p><?= $this->fetch('AppSubTitle') ?></p>
 			</div>
+<?php /*
 			<ul class="app-breadcrumb breadcrumb">
 				<li class="breadcrumb-item"><a href="/admin"><i class="bi bi-house-door fs-6"></i></a></li>
 				<li class="breadcrumb-item"><?= $this->fetch('BreadCrumbTitle') ?></li>
 			</ul>
+*/ ?>
+
 		</div>
--- templates/element/sidebar_menu.php
+++ templates/element/sidebar_menu.php
@@ -12,14 +12,13 @@
 		<ul class="app-menu">
 			
 			<li><?= $this->Html->link('<i class="app-menu__icon bi bi-speedometer"></i><span class="app-menu__label">' . __('Posts') . '</span>', ['controller' => 'Posts', 'action' => 'index'], ['class' => 'app-menu__item', 'escapeTitle' => false]) ?></li>
+			<li><?= $this->Html->link('<i class="app-menu__icon bi bi-speedometer"></i><span class="app-menu__label">' . __('Tests') . '</span>', ['controller' => 'Tests', 'action' => 'index'], ['class' => 'app-menu__item', 'escapeTitle' => false]) ?></li>
 			
 			<li class="treeview">
 			
 				<a class="app-menu__item" href="#" data-toggle="treeview">
 					<i class="app-menu__icon bi bi-laptop"></i><span class="app-menu__label"><?= __('Admin') ?></span><i class="treeview-indicator bi bi-chevron-right"></i>
 				</a>
-
-				
 				<ul class="treeview-menu">
 					<li><a class="treeview-item" href="bootstrap-components.html"><i class="icon bi bi-circle-fill"></i> Bootstrap Elements</a></li>
 					<li>
